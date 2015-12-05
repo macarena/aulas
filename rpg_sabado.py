2964d2365a368ee0b598e99da833eacc4dad4989
Marco Macarena <marcoaasilva@gmail.com>
2015-12-05 12:39:07 -0200

    Sábado fim da aula 05 dez

--- rpg_sabado.py
+++ rpg_sabado.py
@@ -21,33 +21,55 @@ class Monstro(Personagem):
     self.hp = randint(1, jogador.hp)
  
 class Jogador(Personagem):
-  def __init__(self):
-      pass
+  def __init__(self, nome):
+      Personagem.__init__(self)
+      self.estado = 'normal'
+      self.hp = 10
+      self.hp_max = 10
+      self.nome = nome
   def sair(self):
-      pass
+      print "Nada acontece."
   def ajuda(self): 
-      pass
+      print "Nada acontece."
   def status(self):
-      pass
+      print "Nada acontece."
   def cansado(self):
-      pass
+      print "Nada acontece."
   def descansar(self):
-      pass
+      print "Nada acontece."
   def explorar(self):
-      pass
+      print "Nada acontece."
   def fugir(self):
-      pass
+      print "Nada acontece."
   def ataque(self):
-      pass
+      print "Nada acontece."
   def ataque_inimigo(self):
-      pass
+      print "Nada acontece."
  
 Comandos = {
-  'sair': Jogador.sair,
-  'ajuda': Jogador.ajuda,
-  'status': Jogador.status,
-  'descansar': Jogador.descansar,
-  'explorar': Jogador.explorar,
-  'fugir': Jogador.fugir,
-  'atacar': Jogador.ataque,
-  }
+      'sair': Jogador.sair,
+      'ajuda': Jogador.ajuda,
+      'status': Jogador.status,
+      'descansar': Jogador.descansar,
+      'explorar': Jogador.explorar,
+      'fugir': Jogador.fugir,
+      'atacar': Jogador.ataque,
+}
+  
+pergunta = raw_input("Qual o nome do seu personagem? ")
+p = Jogador(pergunta)
+print "(digite ajuda para obter uma lista de comandos)"
+print "%s entra em uma caverna escura, procurando por aventura." % p.nome
+
+while p.hp > 0:
+    linha = raw_input("> ")
+    args = linha.split()
+    if len(args) > 0:
+        comandoEncontrado = False
+        for c in Comandos.keys():
+            if args[0] == c[:len(args[0])]:
+                Comandos[c](p)
+                comandoEncontrado = True
+                break
+        if not comandoEncontrado:
+            print "%s não entende esse comando." % p.nome
