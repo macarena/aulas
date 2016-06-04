6771b3dec960c2dd03aafe06d875f5e9d157b331
Marco Aurélio de Araújo Silva <marcoaasilva@gmail.com>
2016-06-04 12:44:47 -0300

    iniciando classe navio
    aula 04/06

--- batalhanaval/batalha_naval_sabado.py
+++ batalhanaval/batalha_naval_sabado.py
@@ -1,9 +1,17 @@
 from random import randint
 
+class Navio:
+    pass
+
+    def __init__(self, nome):
+        self.nome = nome
+        self.linha = randint(0, linhas -1)
+        self.coluna = randint(0, colunas -1)
+
 tabuleiro = []
 
-colunas = 5
-linhas = 5
+colunas = 10
+linhas = 10
 
 for x in range(linhas):
     tabuleiro.append(["O"] * colunas)
@@ -13,20 +21,21 @@ def print_tabuleiro(t):
     for linha in t:
         print " ".join(linha)
 
-navio_linha = randint(0, linhas -1)
-navio_coluna = randint(0, colunas -1)
+navios = [Navio("titanic"),Navio("bote"),Navio("caravela")]
 
 print "Vamos jogar batalha naval! UHUUUUU!!!"
 
 final = False
 tentativas = 0
 
+print_tabuleiro(tabuleiro)
+
 while final == False:
     print "Você chutou %s vezes" % tentativas
-    tentativas +1
+    tentativas += 1
 
-    chute_linha = input("qual linha?")
-    chute_coluna = input("qual coluna?")
+    chute_linha = input("qual linha?") -1
+    chute_coluna = input("qual coluna?") -1
 
     if chute_linha == navio_linha and chute_coluna == navio_coluna:
         print "PARABÉNS! Acertou mizeravi..."
@@ -34,5 +43,12 @@ while final == False:
 
     else:
         print "Você errou"
+        '''
+        if chute_linha < 0 or chute_coluna < 0 or chute_linha > linhas -1 or chute_coluna > colunas -1:
+            print "Você chutou fora do tabuleiro"
+        else:
+            tabuleiro[chute_linha][chute_coluna] = "X"
+        '''
+
 
     print_tabuleiro(tabuleiro)
