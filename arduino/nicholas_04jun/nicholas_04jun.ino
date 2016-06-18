e0486e2daeb0977ce15f0ea424582dd8daa00565
Marco Aurélio de Araújo Silva <marcoaasilva@gmail.com>
2016-06-18 10:19:11 -0300

    arquivos do nicholas

---
+++ arduino/nicholas_04jun/nicholas_04jun.ino
@@ -0,0 +1,43 @@
+int redi = 13;
+int amareluweed = 12;
+int verdidolly = 11;
+int DYLM = 5;
+int vallorr = 0;
+int tenpu = 0;
+int butum = 0;
+int ystadubutum = 0;
+
+void setup() {
+  pinMode(redi,OUTPUT);
+  Serial.begin(9600);
+  pinMode(amareluweed,OUTPUT);
+  pinMode(verdidolly,OUTPUT);
+  pinMode(butum,INPUT);
+  }
+  // do you love me?
+
+void loop() {
+  vallorr = analogRead(DYLM);
+  ystadubutum = digitalRead(butum);
+  Serial.print(ystadubutum);
+  Serial.print("teste");
+  digitalWrite(redi,HIGH);
+  vallorr = analogRead(DYLM);
+  tenpu = map(vallorr, 0, 1023, 70, 5000);
+  delay(tenpu);
+  digitalWrite(redi,LOW);
+
+  if(ystadubutum == HIGH){
+    digitalWrite(verdidolly,HIGH);
+    vallorr = analogRead(DYLM);
+    tenpu = map(vallorr, 0, 1023, 70, 5000);
+    delay(tenpu);
+    digitalWrite(verdidolly,LOW);
+  }
+  
+  digitalWrite(amareluweed,HIGH);
+  vallorr = analogRead(DYLM);
+  tenpu = map(vallorr, 0, 1023, 70, 5000);
+  delay(tenpu);
+  digitalWrite(amareluweed,LOW);
+}
